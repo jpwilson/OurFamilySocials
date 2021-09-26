93c0ea9cd787bd8c2099ac24d70be79d6c37fc65
jpwilson <jpwilson@hey.com>
2021-09-26 15:44:15 -0500

    wip albums views
    change queryset for homepage
    make homepage list albums by the logged in user

--- albums/admin.py
+++ albums/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Album, Image
 
-admin.site.register(Album)
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "description")
+    fields = ["title", "author", "description"]
+
+
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Image)
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,19 @@
+# from ofs.albums.models import Album
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.contrib.auth import get_user_model
+from albums.models import Album
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    # model = Album  # is shorthand for: queryset = Publisher.objects.all()
+    # user = get_user_model()
+
     template_name = "home.html"
-    context_object_name = "jimbo"
+    context_object_name = "albums"
+
+    def get_queryset(self):
+        return Album.objects.filter(author=self.request.user)
 
 
 class AboutPageView(TemplateView):
