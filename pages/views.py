f9409deb8f0130deb93c0e0b32d2e5ec9428e309
jpwilson <jpwilson@hey.com>
2021-09-30 11:04:29 -0500

    wip - align with whats already there from other workstation

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,12 @@
-# from ofs.albums.models import Album
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth import get_user_model
 from albums.models import Album
 
 
-class HomePageView(ListView):
+# TODO add a homepage for 'not logged in' users...
+class HomePageView(LoginRequiredMixin, ListView):
     # model = Album  # is shorthand for: queryset = Publisher.objects.all()
     # user = get_user_model()
 
