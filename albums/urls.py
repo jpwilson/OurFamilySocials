eefe971bf51230eebe319814f72e0a1813cb4156
jpwilson <jpwilson@hey.com>
2021-09-30 18:53:20 -0500

    sort link for each article listed on homepage

--- albums/urls.py
+++ albums/urls.py
@@ -3,6 +3,6 @@ from .views import add_album_view, album_gallery_view
 
 
 urlpatterns = [
-    path("add/", add_album_view, name="albums"),
-    path("<int:pk>/", album_gallery_view, name="album"),
+    path("add/", add_album_view, name="add_album"),
+    path("<int:pk>/", album_gallery_view, name="view_album"),
 ]
