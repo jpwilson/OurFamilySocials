6f71e2cd7477bd69f8a9ed205e87b6d2a8846269
jpwilson <jpwilson@hey.com>
2021-10-01 11:01:20 -0500

    modify album create page with cancel button (still to be linked up) fix some odd things about layout page?

--- albums/urls.py
+++ albums/urls.py
@@ -3,6 +3,7 @@ from .views import add_album_view, album_gallery_view
 
 
 urlpatterns = [
+    # path("add/", add_album_view, name="add_album"),
     path("add/", add_album_view, name="add_album"),
     path("<int:pk>/", album_gallery_view, name="view_album"),
 ]
