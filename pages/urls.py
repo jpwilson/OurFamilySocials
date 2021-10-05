70b14326c204d3c3a015e1e448cc66c4f2c71950
jpwilson <jpwilson@hey.com>
2021-10-05 10:34:52 -0500

    remove namespacing for the 'pages' app django-allauth does not support namespacing'. Was having trouble with logging in and out.

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView
 
-app_name = "pages"
+# app_name = "pages"
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
