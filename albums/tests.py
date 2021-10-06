86c3076081787024154bcd22649d99d6e14d10f3
jpwilson <jpwilson@hey.com>
2021-10-06 17:26:48 -0500

    new tests for album - created basic test for image and album creation - still need to test the form and templates

--- albums/tests.py
+++ albums/tests.py
@@ -1,6 +1,7 @@
 from django.http import response
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse, resolve
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from .views import add_album_view, edit_album, delete_album, album_gallery_view
 from .models import Album, Image
@@ -9,6 +10,8 @@ from .models import Album, Image
 #       as shown here: https://realpython.com/testing-in-django-part-1-best-practices-and-examples/
 # TODO use factoryboy
 # TODO test the forms and formsets...
+
+
 class AddAlbumTests(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
@@ -23,6 +26,18 @@ class AddAlbumTests(TestCase):
             blog="This is the contents of the blog field of the test album",
         )
 
+        Image.objects.create(
+            image=SimpleUploadedFile(
+                "file.jpg", b"file_content", content_type="image/jpeg"
+            ),
+            caption="Test Image Caption",
+            album=Album.objects.get(id=1),
+        )
+
     def test_album_info(self):
         album = Album.objects.get(id=1)
         self.assertEqual("{}".format(album.title), "My Test Album")
+
+    def test_image_info(self):
+        image = Image.objects.get(id=1)
+        self.assertEqual("{}".format(image.caption), "Test Image Caption")
