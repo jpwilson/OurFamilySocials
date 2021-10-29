ad22b50a59bd9cf182f2be780a0c7a1a820556a1
jpwilson <jpwilson@hey.com>
2021-10-28 19:12:00 -0500

    feat: add unrelate method This is identical the fb's 'unfriend' metho

--- family/models.py
+++ family/models.py
@@ -30,3 +30,16 @@ class FamilyList(models.Model):
         """
         if account in self.relatives.all():
             self.relatives.remove(account)
+
+    def un_relate(self, removee):
+        """
+        Dont confuse the two users (FriendList.user and removee's list.user , with the two lists (self , and removee's )
+        This takes into account the remover (initiates this) and the removee
+        That is, 'remove_relative' has to be called on both parties
+        """
+        remover_relative_list = self
+
+        remover_relative_list.remove_relative(removee)
+
+        removee_relative_list = FamilyList.objects.get(user=removee)
+        removee_relative_list.remove_relative(self.user)
