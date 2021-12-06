ecc263e30e85d177aa6ceb209784012cfa59caac
jpwilson <jpwilson@hey.com>
2021-12-06 15:24:57 -0600

    add context to profile modify the template of profile to have 2 querysets modify the CBV (ProfileView) to have useful information but adding queries to get_context_data

--- pages/views.py
+++ pages/views.py
@@ -23,3 +23,14 @@ class AboutPageView(TemplateView):
 class ProfileView(DetailView):
     model = get_user_model()
     template_name = "account/profile.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context["your_recent_albums"] = Album.objects.filter(
+            author=self.request.user
+        ).order_by("-pub_date")[:3]
+        context["all_albums"] = Album.objects.all()
+
+        return context
